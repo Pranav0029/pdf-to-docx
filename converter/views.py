ddd88727e98f4d99b5246130c026a1a1743ae590
Pranav <pranavgawade026@gmail.com>
2026-02-10 15:15:14 +0530

    Updated PDF to DOCX converter with file size limit

---
+++ converter/views.py
@@ -0,0 +1,50 @@
+import os
+from django.shortcuts import render
+from django.http import HttpResponse
+from pdf2docx import Converter
+import tempfile
+
+def home(request):
+    if request.method == "POST":
+        uploaded_file = request.FILES['pdf_file']
+
+        MAX_FILE_SIZE = 3 * 1024 * 1024  # 3 MB
+
+        if uploaded_file.size > MAX_FILE_SIZE:
+            return HttpResponse(
+                "❌ File size too large. Please upload PDF under 3 MB"
+                " this is just practice project .",
+                status=400
+            )
+
+        name_only, _ = os.path.splitext(uploaded_file.name)
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
+            for chunk in uploaded_file.chunks():
+                temp_pdf.write(chunk)
+            temp_pdf_path = temp_pdf.name
+
+        docx_file_path = temp_pdf_path.replace(".pdf", ".docx")
+
+        try:
+            cv = Converter(temp_pdf_path)
+            cv.convert(docx_file_path, start=0, end=None)
+            cv.close()
+
+            with open(docx_file_path, 'rb') as f:
+                docx_data = f.read()
+
+            response = HttpResponse(
+                docx_data,
+                content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            )
+            response["Content-Disposition"] = f'attachment; filename="{name_only}.docx"'
+            return response
+
+        finally:
+            if os.path.exists(temp_pdf_path):
+                os.remove(temp_pdf_path)
+            if os.path.exists(docx_file_path):
+                os.remove(docx_file_path)
+
+    return render(request, "index.html")
